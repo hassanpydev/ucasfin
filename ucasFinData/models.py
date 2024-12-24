5fa07705b98c544718ca4bd36a07f3a146036698
hassanpydev <hassangames1998@gmail.com>
2024-12-24 13:54:13 +0200

    improvments

--- ucasFinData/models.py
+++ ucasFinData/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Employee(models.Model):
-    _tablename_ = 'employees'
+
     employee_id = models.AutoField(primary_key=True)
     emp_num = models.IntegerField()
     name = models.CharField(max_length=250)
@@ -11,5 +11,12 @@ class Employee(models.Model):
     account_number = models.CharField(max_length=250)
     iban = models.CharField(max_length=250)
     is_valid = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class User(models.Model):
+    username = models.CharField(max_length=250)
+    password = models.CharField(max_length=250)
+    emp_info = models.ForeignKey(Employee,on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now=True)
     updated_at = models.DateTimeField(auto_now=True)
--- ucasFinData/views.py
+++ ucasFinData/views.py
@@ -1,7 +1,14 @@
+from http.client import responses
+
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Employee
-# Create your views here.
+from .models import Employee,User
+#
+from hashlib import md5
+def hasher(data):
+    # hash data to be stored in cookies
+    print(md5(data.encode()).hexdigest())
+    return md5(data.encode()).hexdigest()
 def index(request):
     if request.method == "POST":
         employment_number = request.POST.get("employmentNumber")
@@ -9,15 +16,30 @@ def index(request):
         emp = Employee.objects.filter(emp_num=employment_number , employee_id=id_number).first()
         print(emp)
         if emp:
-            return render(request,"dataFin/user_data.html",context={"emp":emp})
+            if request.session.get("emp_num") is not None:
+                del request.session["emp_num"]
+                request.session["emp_num"] = hasher(str(str(emp.emp_num) + str(emp.employee_id)) )
+            # check if user is valid
+            if emp.is_valid:
+                return render(request,"dataFin/index.html",context={"msg":"لا يمكن تعديل بياناتك"})
+            else:
+                # check if user has iban
+                if not emp.iban:
+                    return render(request,"dataFin/edit_client_data.html",context={"emp":emp})
+
+
+                return render(request,"dataFin/user_data.html",context={"emp":emp})
         else:
-            return HttpResponse("User Not Found")
+            return render(request,"dataFin/index.html",context={"msg":"البيانات غير صحيحة"})
 
     return render(request,"dataFin/index.html")
 
 def edit_client(request,id):
+
     try:
         emp = Employee.objects.get(emp_num=id)
+        if request.session.get("emp_num") is None or request.session.get("emp_num") != hasher(str(emp.emp_num) + str(emp.employee_id)):
+            return render(request,"dataFin/index.html",context={"msg":"الرجاء التاكد من الرقم الوظيفي"})
         if emp.is_valid:
             return render(request,"dataFin/edit_client_data.html",context={"msg":"لم يمكن تعديل بياناتك"})
         return render(request,"dataFin/edit_client_data.html",context={"emp":emp})
@@ -32,21 +54,38 @@ def save_client_data(request):
         emp_id = request.POST.get("id")
         bank = request.POST.get("bank")
         branch = request.POST.get("branch")
-        obj = Employee.objects.get(emp_num=emp_num)
-        obj.name = name
-        obj.iban = iban
-        obj.employee_id = emp_id
-        obj.bank = bank
-        obj.branch = branch
-        obj.is_valid = True
-        obj.save()
-        return render(request,"dataFin/edit_client_data.html")
-
+        if request.session.get("emp_num") == hasher(str(emp_num) + str(emp_id)):
+            obj = Employee.objects.get(emp_num=emp_num)
+            obj.name = name
+            obj.iban = iban
+            obj.employee_id = emp_id
+            obj.bank = bank
+            obj.branch = branch
+            obj.is_valid = True
+            obj.save()
+            return render(request,"dataFin/edit_client_data.html")
+        else:
+            return render(request,"dataFin/index.html",context={"msg":"الرجاء التاكد من الرقم الوظيفي"})
+    return render(request,"dataFin/index.html")
 def mark_valid(request,id):
     try:
-        emp = Employee.objects.get(emp_num=id)
-        emp.is_valid = True
-        emp.save()
-        return render(request, "dataFin/edit_client_data.html", context={"msg": "تم تأكيد بياناتك بنجاح"})
+        if request.session.get("emp_num") == id:
+            emp = Employee.objects.get(emp_num=id)
+            emp.is_valid = True
+            emp.save()
+            return render(request, "dataFin/edit_client_data.html", context={"msg": "تم تأكيد بياناتك بنجاح"})
     except Employee.DoesNotExist:
         return render(request, "dataFin/edit_client_data.html", context={"msg": "الرجاء التاكد من الرقم الوظيفي"})
+
+
+# def migrator(request):
+#     emp_obj = Employee.objects.all()
+#     print(len(emp_obj))
+#     for emp in emp_obj:
+#         branch = emp.bank_name.split(" (")
+#         if len(branch) == 2:
+#             emp.bank_name = branch[0]
+#             emp.branch = branch[1].replace(")","")
+#             emp.save()
+#     return HttpResponse("Done")
+#
